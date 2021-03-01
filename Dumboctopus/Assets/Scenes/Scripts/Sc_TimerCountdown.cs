83f62f69e3f4a20b7cef4ebba93992ea99468159
Myrandagau <73671431+Myrandagau@users.noreply.github.com>
2021-02-28 19:56:06 -0500

    timer + slider

---
+++ Dumboctopus/Assets/Scenes/Scripts/Sc_TimerCountdown.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Sc_TimerCountdown : MonoBehaviour
+{
+    public float timeRemaining;
+    public float maxTime = 210;
+    public bool timerIsRunning = false;
+    public Text timeText;
+    public Slider slider;
+    private void Start()
+    {
+        timerIsRunning = true;
+        timeRemaining = maxTime;
+    }
+
+
+    void Update()
+    {
+        if (timerIsRunning)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+                DisplayTime();
+            }
+            else
+            {
+                timeRemaining = 0;
+                timerIsRunning = false;
+            }
+        }
+        UpdateSlider();
+    }
+
+    void DisplayTime()
+    {
+        float minutes = Mathf.FloorToInt(timeRemaining / 60);
+        float seconds = Mathf.FloorToInt(timeRemaining % 60);
+
+        timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
+    void UpdateSlider ()
+    {
+
+        slider.value = timeRemaining / maxTime;
+    }
+}
